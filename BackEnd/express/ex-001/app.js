4e3ca774e4248a07fe3662683ceac9d40c1a6d1c
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2024-09-13 21:43:46 -0700

    my first server using express

--- BackEnd/express/ex-001/app.js
+++ BackEnd/express/ex-001/app.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+import express from "express";
+const app = express();
+
+const port = process.env.port || 3000;
+app.listen(port, ()=> {
+    console.log(`server is running on <http://localhost:${port}`);
+    
+})
