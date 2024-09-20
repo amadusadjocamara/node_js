670e7036c3867261d021ab051ad17ff263c9dd41
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2024-09-20 13:48:52 -0700

    first commit ex-007 preperinda apis data

---
+++ BackEnd/express/ex-007/app.js
@@ -0,0 +1,3 @@
+const {porducts} = require('./data')
+
+console.log(porducts);
---
+++ BackEnd/express/ex-007/data.js
@@ -0,0 +1,62 @@
+const porducts =[
+    {
+        id: 1,
+        name: 'Wireless Bluetooth Earbuds',
+        image: 'https://www.audioshop.in/wp-content/uploads/6ff70f66ce935f02861635b3e936a70c.jpeg',
+        price: 49.99,
+        desc: 'Compact, high-quality earbuds with noise-cancellation and 20-hour battery life. Perfect for on-the-go listening.'
+    },
+    {
+        id: 2,
+        name: 'Smartwatch Pro',
+        image: 'https://images.indianexpress.com/2023/10/cmf-watch-pro-review-featured.jpg',
+        price: 199.99,
+        desc: 'A feature-rich smartwatch with fitness tracking, heart rate monitoring, GPS, and a sleek, modern design'
+    },
+    {
+        id: 3,
+        name: '4K Ultra HD TV (55-inch)',
+        image: 'https://m.media-amazon.com/images/I/71DGhNHPyeL.jpg',
+        price: 699.99,
+        desc: 'Enjoy stunning visuals with this 55-inch 4K Ultra HD TV. Supports HDR10 for lifelike colors and contrast.'       
+    },
+    {
+        id: 4,
+        name: 'Portable Power Bank (20,000mAh)',
+        image: 'https://urbnworld.com/cdn/shop/files/20_000mAhNanopurple-05.webp?v=1702893131',
+        price: 29.99,
+        desc: 'Charge your devices on the go with this high-capacity power bank, compatible with most smartphones and tablets.'
+    },
+    {
+        id: 5,
+        name: 'Electric Standing Desk',
+        image: 'https://5.imimg.com/data5/MR/FJ/EG/SELLER-15573091/electric-standing-desk-500x500.jpg',
+        price: 399.99,
+        desc: 'Adjustable electric standing desk with memory presets and ergonomic design to improve posture and productivity.'
+    },
+    {
+        id: 6,
+        name: 'Gaming Laptop (16GB RAM, 512GB SSD)',
+        image: 'https://m.media-amazon.com/images/I/81wl5P7kuAL._AC_UF1000,1000_QL80_.jpg',
+        price: 1.2999 ,
+        desc: ' High-performance gaming laptop with a powerful processor and dedicated graphics card for smooth gameplay.'
+    },
+    {
+        id: 7,
+        name: 'Noise-Cancelling Headphones',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6SHlAp8dRGUZzRm5uCDJFpPJMVLTqu6_r9A&s',
+        price: 159.9,
+        desc: ' Premium over-ear headphones with active noise-cancellation, perfect for immersive audio experiences.',
+    },
+    {
+        id: 8,
+        name: 'Digital Air Fryer (5.8 Quart)',
+        image: 'https://images-cdn.ubuy.co.in/633b4d4b2297521c0d74ee14-ubuy-online-shopping.jpg',
+        price: 89.30,
+        desc: 'Healthier cooking made easy with this digital air fryer. Features multiple preset modes for crispy, oil-free meals.'
+
+    }
+
+]
+
+module.exports={porducts};
