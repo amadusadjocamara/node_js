dc3f2b0146735fcde2033e0babcd0b72f1012901
Amadu Sadjo Camara <105577816+amadusadjocamara@users.noreply.github.com>
2024-09-21 04:12:24 -0700

    ex-007

--- BackEnd/express/ex-007/app.js
+++ BackEnd/express/ex-007/app.js
@@ -1,3 +1,12 @@
-const {porducts} = require('./data')
+const express = require('express');
+const {porducts} = require('./data');
+const app = express();
+const port = 5000
 
-console.log(porducts);
+app.get('/', (req, res)=>{
+    res.send({porducts})
+})
+
+app.listen(port, (req, res)=> {
+    console.log(`Server is running on <http:localhost:${port}>`);
+});
